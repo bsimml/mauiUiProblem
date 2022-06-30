a04902f90310a1254496283f0fe4595480240c4d
bsimml <benjamin.simml@ds-content.de>
2022-06-30 10:13:28 +0200

    UI error test project

---
+++ MauiApp2/MainPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Toolkit.Mvvm.Input;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace MauiApp2
+{
+    public class MainPageViewModel : ObservableObject
+    {
+        public IRelayCommand ClickedCommand { get; set; }
+
+
+        private bool _isWorking;
+
+        public bool IsWorking
+        {
+            get { return _isWorking; }
+            set { SetProperty(ref _isWorking, value); }
+        }
+
+        public MainPageViewModel()
+        {
+            ClickedCommand = new RelayCommand(Clicked);
+
+            // If false "space" for Label is never created only apears after orientation change
+            //IsWorking = false;
+
+            // If true "space" is created and visibility can be toggled
+            IsWorking = true;
+        }
+
+        private void Clicked() 
+        {
+            if (IsWorking)
+                IsWorking = false;
+            else
+                IsWorking = true;
+        }
+
+    }
+}
---
+++ MauiApp2/MauiProgram.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Maui;
+
+namespace MauiApp2;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+        builder.UseMauiApp<App>().UseMauiCommunityToolkit();
+        builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+		builder.Services.AddSingleton<MainPageViewModel>();
+		builder.Services.AddSingleton<MainPage>();
+
+		return builder.Build();
+	}
+}
